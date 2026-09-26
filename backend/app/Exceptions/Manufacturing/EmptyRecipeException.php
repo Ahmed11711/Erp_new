f44782de033dde8c6707bab1d1dd741cfaaab2c7
shaimaa123 <shaimaashorih@gmail.com>
2026-09-26 09:46:10 +0300

    delete1

--- backend/app/Exceptions/Manufacturing/EmptyRecipeException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Exceptions\Manufacturing;
-
-use RuntimeException;
-
-final class EmptyRecipeException extends RuntimeException
-{
-}
--- backend/app/Repositories/AccountTree/AccountTreeRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\AccountTree;
-
-use App\Repositories\BaseRepository\BaseRepositoryInterface;
-
-interface AccountTreeRepositoryInterface extends BaseRepositoryInterface
-{
- }
